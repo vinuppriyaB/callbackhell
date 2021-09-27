750ad9e586f7eb452b013999f3a106ab7b3cbf6f
vinuppriya <vinuppriya.b@gmail.com>
2021-09-27 14:55:38 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,57 @@
+
+
+setTimeout(function(e)
+{
+    document.getElementById("display").innerHTML="10";
+
+}, 1000);
+setTimeout(function(e)
+{
+    document.getElementById("display").innerHTML="9";
+
+}, 2000);
+setTimeout(function(e)
+{
+    document.getElementById("display").innerHTML="8";
+
+}, 3000);
+setTimeout(function(e)
+{
+    document.getElementById("display").innerHTML="7";
+
+}, 4000);
+setTimeout(function(e)
+{
+    document.getElementById("display").innerHTML="6";
+
+}, 5000);
+setTimeout(function(e)
+{
+    document.getElementById("display").innerHTML="5";
+
+}, 6000);
+setTimeout(function(e)
+{
+    document.getElementById("display").innerHTML="4";
+
+}, 7000);
+setTimeout(function(e)
+{
+    document.getElementById("display").innerHTML="3";
+
+}, 8000);
+setTimeout(function(e)
+{
+    document.getElementById("display").innerHTML="2";
+
+}, 9000);
+setTimeout(function(e)
+{
+    document.getElementById("display").innerHTML="1";
+
+}, 10000);
+setTimeout(function(e)
+{
+    document.getElementById("display").innerHTML="Happy Independence Day";
+
+}, 11000);
